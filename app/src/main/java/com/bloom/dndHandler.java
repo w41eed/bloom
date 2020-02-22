fd1a1bdc027c6b2f88a92cfa3a854342fe0db44a
Waleed <w41eed@outlook.com>
2020-02-22 14:58:52 -0500

    1. added dndHandler.java 2. Add strings to strings.xml 3. changed api to 23 4. added notification permission in Manifest

---
+++ app/src/main/java/com/bloom/dndHandler.java
@@ -0,0 +1,91 @@
+package com.bloom;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import static java.sql.Types.NULL;
+
+
+
+
+
+
+    public class dndHandler {
+
+        public boolean dndPermission = false;
+        private NotificationManager nManager;
+        private Activity mainActivity;
+
+
+        //Constructor
+        //Use inside onCreate of MainActivity
+        public dndHandler(Activity activity){
+            mainActivity = activity;
+            nManager = (NotificationManager) mainActivity.getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+
+        //Checks if app has access to Do not disturb
+        //If no access then prompts user to give permission
+        //Call every time before you access Do not disturb
+        public void checkDndPermission(){
+
+            if (!nManager.isNotificationPolicyAccessGranted()) {
+                dndPermission = false;
+                //Ask for permission
+                dndPermissionDialog();
+            }
+            else {
+                dndPermission = true;
+            }
+        }
+
+
+        //Dialog box for permission
+        public void dndPermissionDialog(){
+
+            //Dialog box to prompt user for permission
+            AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+            builder.setMessage(R.string.dnd_dialog_message).setTitle(R.string.dnd_dialog_title);
+
+            //If user agrees to grant access then move to notification settings
+            builder.setPositiveButton(R.string.dnd_dialog_allow, new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int id){
+                    //After clicks 'Allow'
+                    Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                    mainActivity.startActivity(intent);
+                    mainActivity.overridePendingTransition(NULL, NULL);
+
+                }
+            });
+
+            //If user doesn't allow access
+            builder.setNegativeButton(R.string.dnd_dialog_cancel, new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int id) {
+                    //After user clicks 'Do Not Allow'
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+
+        //Will turn ON Do Not Disturb
+        public void turnOnDnd(){
+            nManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE);
+        }
+
+
+        //Will turn OFF Do Not Disturb
+        public void turnOffDnd(){
+            nManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL);
+        }
+
+
+
+    }
+
